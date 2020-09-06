86e76621edf7bd2afdb547bd15ae23c1e2e37c8b
Sergio Bosch Vega <sergio.bosch@gmail.com>
2020-09-06 20:46:08 +0200

    Delete pie.vue

--- componentes/pie.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <footer class="footer py-4 bg-secondary text-white">
-      <div class="container text-center">
-        <small>Copyright &copy; <strong>2020</strong> Sergio Bosch Vega </small>
-      </div>
-    </footer>
-  </div>
-</template>

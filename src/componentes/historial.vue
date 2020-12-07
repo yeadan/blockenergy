c3ee344f5f511f4581dc416a611f23a3f89ba5f4
Sergio Bosch Vega <sergio.bosch@gmail.com>
2020-12-07 14:13:19 +0100

    Add files via upload

--- src/componentes/historial.vue
+++ src/componentes/historial.vue
@@ -1,12 +1,12 @@
 <template>
-      <div class="container">
+      <div class="container" style="position: relative; height: auto !important; min-height: 100%">
         <div class="row" v-if="Object.keys(historial).length > 0">
-          <div class="d-flex align-items-stretch min-vw-75 max-hw-100" v-for="(oferta, index) in showHechoTrue" :key="index">
+          <div class="col-xl-3 col-lg-4 col-md-6 col-sd-6" v-for="(oferta, index) in showHechoTrue" :key="index">
             <div class="card mb-2 text-center col-11" >
-              <div style="position:relative">
+              <div class="iblock">
                 <img class="card-img-top img-center align-items-stretch"
                   v-bind:src="blockie(oferta.vendedor)" v-bind:title="oferta.vendedor" >
-                <div style="letter-spacing: 0.05em;color:white;position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);" v-bind:title="oferta.vendedor">Vendedor </div>
+                <div class="icentered" v-bind:title="oferta.vendedor">Vendedor </div>
               </div>
               <div class="card-block">
                 <p align="left" class="card-text"><small><strong>Precio: </strong>{{ oferta.precio }}€/kWh</small></p>
@@ -14,9 +14,9 @@
                 <p align="left" class="card-text"><small><strong>Total: </strong>{{(oferta.cantidad/1000*oferta.precio).toFixed(2)}}€</small></p>
                 <p align="left" class="card-text"><small><strong>Eth: </strong>{{(oferta.gwei/1000000000).toFixed(7)}}Ξ </small></p>                  
                 <p align="left" class="card-text"><small><strong>Fecha: </strong>{{ timestampToDate(oferta.id) }}</small></p>                  
-                <div style="position:relative">
+                <div class="iblock">
                   <img class="card-img"  v-bind:src="blockie(oferta.comprador)" v-bind:title="oferta.comprador" width="2">
-                  <div style="color:white;position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%)" v-bind:title="oferta.comprador">Comprador </div>
+                  <div class="icentered" v-bind:title="oferta.comprador">Comprador </div>
                 </div>
               </div>
             </div>
@@ -97,5 +97,19 @@ img {
 }
 h2 {
   margin-bottom:15%
+}
+.iblock {
+  position: relative;
+  text-align: center;
+  color: white;
+}
+
+/* Centered text */
+.icentered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  letter-spacing: 0.05em
 }
 </style>
